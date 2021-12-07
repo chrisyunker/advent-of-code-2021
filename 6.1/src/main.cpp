a64edb50e4f7c1a0c256d5c44672036af62ea3c9
Chris Yunker <chrisyunker@gmail.com>
2021-12-06 21:28:26 -0600

    Day 6.1

--- 6.1/src/main.cpp
+++ 6.1/src/main.cpp
@@ -6,74 +6,61 @@
 #include <map>
 #include <iostream>
 
-struct coord
+std::vector<int> parseInput(std::ifstream &fd)
 {
-    int x;
-    int y;
-};
+    std::vector<int> numbers;
 
-void parseInput(std::ifstream &fd, std::vector<int*>& rows)
-{
-    std::string del = "->, \n";
+    const char del = ',';
 
     std::string line;
     while (std::getline(fd, line))
     {
-        int *row = new int[4]; 
-        int i = 0;
-        size_t pos;
-        size_t prev = 0;
-        while ((pos = line.find_first_of(del, prev)) != std::string::npos)
-        {
-            if (pos > prev)
-            {
-                int num = std::stoi(line.substr(prev, pos - prev));
-                row[i++] = num;
-            }
-            prev = pos + 1;
-        }
-        if (prev < line.length())
-        {
-            int num = std::stoi(line.substr(prev, std::string::npos));
-            row[i++] = num;
-        }
-        rows.push_back(row);
-    }
-};
 
-int count(int min, int max, int** board)
-{
-    int total = 0;
-    for (int y = 0; y <= max; y++)
-    {
-        for (int x = 0; x <= max; x++)
+        size_t start;
+        size_t end = 0;
+        while ((start = line.find_first_not_of(del, end)) != std::string::npos)
         {
-            if (board[x][y] >= min)
-            {
-                total += 1;
-            }
+            end = line.find(del, start);
+            numbers.push_back(std::stoi(line.substr(start, end - start)));
         }
+
     }
-    return total;
+    return numbers;
 }
 
-void print(int max, int** board)
+struct Fish
 {
-    for (int y = 0; y <= max; y++)
+    Fish(int t) : timer{t}
     {
-        for (int x = 0; x <= max; x++)
+    }
+
+    bool IncrementDay()
+    {
+        if (timer == 0)
         {
-            printf("[%d]", board[x][y]);
+            timer = 6;
+            return true;
         }
-        printf("\n");
+        timer -= 1;
+        return false;
+    }
+
+    int timer;
+};
+
+void printFish(int day, const std::vector<Fish>& fish)
+{
+    printf("Day: %d : ", day);
+    for (const Fish& f: fish)
+    {
+        printf("%d ", f.timer);
     }
     printf("\n");
 }
 
-
 int main(int argc, char* argv[])
 {
-    const int numArgs = 1;
+    const int numArgs = 2;
 
     if ((argc - optind) < numArgs)
     {
@@ -82,116 +69,48 @@ int main(int argc, char* argv[])
     }
 
     std::string input(argv[optind]);
+    int days = std::stoi(argv[optind + 1]);
 
     printf("input file: %s\n", input.c_str());
+    printf("input days: %d\n", days);
 
     std::ifstream fd(input);
    
-    std::vector<int*> rows;
-    parseInput(fd, rows);
+    auto numbers = parseInput(fd);
+    std::vector<Fish> fish;
 
-    printf("Number of numbers: %lu\n", rows.size());
+    printf("Number of numbers: %lu\n", numbers.size());
 
-    int max = 0;
-    for (auto row : rows)
+    for (auto n : numbers)
     {
-        for (int i = 0; i < 4; i++)
-        {
-            if (row[i] > max)
-                max = row[i];
-        }
+        Fish f{ n };
+        fish.push_back(f);
     }
-    printf("max: %d\n", max);
 
-    int **board = new int*[max];
-
-    for (int x = 0; x <= max; x++)
+    for (int i = 0; i < days; i++)
     {
-        board[x] = new int[max];
-        for (int y = 0; y <= max; y++)
+        //printf("Day: %d\n", i);
+
+        std::vector<Fish> newFish;
+        for (Fish& f : fish)
         {
-            //printf("zero: [%d][%d]\n", x, y);
-            board[x][y] = 0;
+            if (f.IncrementDay())
+            {
+                //printf("New Fish\n");
+                Fish nf{ 8 };
+                newFish.push_back(std::move(nf));
+            }
         }
+        for (const Fish& f : newFish)
+        {
+            fish.push_back(f);
+        }
+        printFish(i, fish);
     }
+
     
-    for (auto row : rows)
-    {
-        //printf("[%d][%d][%d][%d]\n", row[0], row[1], row[2], row[3]);
 
-        int x1 = row[0];
-        int y1 = row[1];
-        int x2 = row[2];
-        int y2 = row[3];
-        //printf("Add line: %d,%d -> %d,%d\n", x1,y1,x2,y2);
-
-        if (x1 == x2)
-        {
-            if (y2 > y1)
-            {
-                for (int i = y1; i <= y2; i++)
-                {
-                    //printf("incy: [%d][%d]\n", x1, i);
-                    board[x1][i] += 1;
-                }
-            }
-            else
-            {
-                for (int i = y2; i <= y1; i++)
-                {
-                    //printf("incy: [%d][%d]\n", x1, i);
-                    board[x1][i] += 1;
-                }
-            }
-        }
-        else if (y1 == y2)
-        {
-            if (x2 > x1)
-            {
-                for (int i = x1; i <= x2; i++)
-                {
-                    //printf("incx: [%d][%d] val:%d\n", i, y1, board[i][y1]);
-                    board[i][y1] += 1;
-                }
-            }
-            else
-            {
-                for (int i = x2; i <= x1; i++)
-                {
-                    //("incx: [%d][%d] val:%d\n", i, y1, board[i][y1]);
-                    board[i][y1] += 1;
-                }
-            }
-        }
-        else
-        {
-            if (abs(x1 - x2) == abs(y1 - y2))
-            {
-                while (x1 != x2)
-                {
-                    //printf("diag: [%d][%d]\n", x1, y1);
-
-                    board[x1][y1] += 1;
-
-                    if (x1 > x2)
-                        x1--;
-                    else
-                        x1++;
-
-                    if (y1 > y2)
-                        y1--;
-                    else
-                        y1++;
-                }
-                board[x2][y2] += 1;
-            }
-        }
-    }
-
-    print(max, board);
-
-    int overlap = count(2, max, board);
-    printf("Overlap: %d\n", overlap);
+    printf("Number of fish: %lu\n", fish.size());
 
     return 0;
 }
